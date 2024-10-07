84a1c167896b31bf8bb10456e1c0d026452abacb
Continuous Integration <>
2024-10-07 19:24:08 +0000

    Update pay javadoc

--- pay/javadoc/package-search-index.js
+++ pay/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.solana.pay"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.solana.pay"}];updateSearchResults();
